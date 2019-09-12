efb7a8d6c856e3211a30e1dced05de20d562be78
Juan Chavez <juanchavez017@gmail.com>
2019-09-11 18:06:23 -0700

    Added JWT token authentication to login

--- routes/login.js
+++ routes/login.js
@@ -2,24 +2,40 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcryptjs');
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = 'super_secret_jwt_passcode';
 
 module.exports = function() {
   let router = express.Router();
 
   router.get('/', bodyParser.json(), async (req, res, next) => {
     let { username, password } = req.query;
-    let user = await User.findOne({ 'username': username }).exec();
+    let user = await User.findOne({ username: username }).exec();
     if (!user) {
-      res.status(400).send('Error logging in. Check username.');
+      res
+        .status(400)
+        .send({ success: false, error: 'Error logging in. Check username.' });
       return;
     }
+
     let pwTest = await bcrypt.compare(password, user.password);
-    if(!pwTest){
-        res.status(400).send('Error logging in. Check password.');
-        return;
+    if (!pwTest) {
+      res
+        .status(400)
+        .send({ success: false, error: 'Error logging in. Check password.' });
+      return;
     }
-    
-    res.status(200).send({ username, password });
+
+    let expiration = new Date().setUTCDate(new Date().getUTCDate() + 30);
+    let token = jwt.sign(
+      {
+        exp: expiration / 1000,
+        user: username
+      },
+      JWT_SECRET
+    );
+
+    res.status(200).send({ success: true, token });
   });
 
   return router;
