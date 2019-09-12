2668a5334efd880aa1b4e8277447417649404c82
Juan Chavez <juanchavez017@gmail.com>
2019-09-11 18:02:11 -0700

    Added bcrypt password validation to the login function

--- routes/login.js
+++ routes/login.js
@@ -1,13 +1,26 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');
+const User = require('../models/user');
 
-module.exports = function(config){
-    let router = express.Router();
+module.exports = function() {
+  let router = express.Router();
 
-    router.get('/', bodyParser.json(), async (req, res, next) => {
-        let {username, password} = req.query;
-        res.status(200).send({username, password});
-    });
+  router.get('/', bodyParser.json(), async (req, res, next) => {
+    let { username, password } = req.query;
+    let user = await User.findOne({ 'username': username }).exec();
+    if (!user) {
+      res.status(400).send('Error logging in. Check username.');
+      return;
+    }
+    let pwTest = await bcrypt.compare(password, user.password);
+    if(!pwTest){
+        res.status(400).send('Error logging in. Check password.');
+        return;
+    }
+    
+    res.status(200).send({ username, password });
+  });
 
-    return router;
-}
+  return router;
+};
